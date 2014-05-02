8053a9095e24f7df1252fdc50c11245ff763f882
Jeremy Greer <jeremygreer@reergymerej.com>
2014-05-02 12:48:53 -0400

    Add bower, provide middleware to serve bower components

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// allow access to bower components
+app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
+
 app.use(app.router);
 
 // Watch for requests for html instead of jade templates.
